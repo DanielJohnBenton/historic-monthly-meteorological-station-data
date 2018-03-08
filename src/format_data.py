f202241088505514a5be0d0669ad3f54e22a42bc
Daniel John Benton <danieljbwork@gmail.com>
2018-03-08 00:08:02 +0000

    format_data script

--- src/format_data.py
+++ src/format_data.py
@@ -1,25 +1,57 @@
 from csv import DictReader
 from deduplicate_whitespace import deduplicate_whitespace
 
-EXPECTED_CHARS = "0123456789-.#*"
+NUMBERS_ONLY = "0123456789-."
+
+def resolve_number(text, isSunHours=False):
+	if text == "---":
+		if isSunHours:
+			return "", "N", "Campbell Stokes recorder"
+		else:
+			return "", "N"
+	
+	estimated = "N"
+	measurementVia = "Campbell Stokes recorder"
+	numbersOnly = ""
+	
+	for character in text:
+		if character in NUMBERS_ONLY:
+			numbersOnly += character
+	
+	if "*" in text:
+		estimated = "Y"
+	if "#" in text:
+		measurementVia = "automatic Kipp & Zonen sensor"
+	
+	if isSunHours:
+		return numbersOnly, estimated, measurementVia
+	else:
+		return numbersOnly, estimated
 
 with open("formatted_data/locations_final.csv", "r", encoding="utf-8") as locationsCsvFile:
 	refs = [row["ref"] for row in DictReader(locationsCsvFile)]
 	
 for ref in refs:
-	print(ref)
-	
 	with open(f"original_txt/{ref}.txt", "r", encoding="utf-8") as inFile:
 		text = deduplicate_whitespace(inFile.read())
+	lines = [line.strip().split(" ") for line in text.split("hours\n")[1].splitlines()]
 	
-	lines = [line.strip() for line in text.split("hours\n")[1].splitlines()]
+	output = "yyyy,mm,tmax_degc,tmin_degc,af_days,rain_mm,sun_hours,sun_hours_measurement_via,tmax_degc_estimated,tmin_degc_estimated,af_days_estimated,rain_mm_estimated,sun_hours_estimated,provisional"
+	
 	for line in lines:
-		printedLine = False
-		for piece in line.split(" "):
-			for character in piece:
-				if character not in EXPECTED_CHARS:
-					print(f"\t{line}")
-					printedLine = True
-					break
-			if printedLine:
-				break
+		while len(line) < 8:
+			line.append("---")
+		
+		yyyy = line[0]
+		mm = line[1]
+		tmax_degc, tmax_degc_estimated = resolve_number(line[2])
+		tmin_degc, tmin_degc_estimated = resolve_number(line[3])
+		af_days, af_days_estimated = resolve_number(line[4])
+		rain_mm, rain_mm_estimated = resolve_number(line[5])
+		sun_hours, sun_hours_estimated, sun_hours_measurement_via = resolve_number(line[6], isSunHours=True)
+		provisional = "Y" if line[7].upper() == "PROVISIONAL" else "N"
+		
+		output += f"\n{yyyy},{mm},{tmax_degc},{tmin_degc},{af_days},{rain_mm},{sun_hours},{sun_hours_measurement_via},{tmax_degc_estimated},{tmin_degc_estimated},{af_days_estimated},{rain_mm_estimated},{sun_hours_estimated},{provisional}"
+	
+	with open(f"output/{ref}.csv", "w", encoding="utf-8") as outFile:
+		outFile.write(output)
